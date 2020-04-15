8520a4382b8ea873314c41b78ba7aaaff5560d04
natek <natekinderman16@gmail.com>
2020-04-15 14:11:30 -0500

    Improved the quotes and made the topic about funny/wise verse tech ideas

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
-// Data for quotes.
-// This creates a variable called quotes. This variable is an array of objects. Each object includes a quote and the name of the person that quote is attributed to. 
-
 const quotes = [
     {
-        quote: "That brain of mine is something more than merely mortal; as time will show.",
-        name: "Ada Lovelace, Founder of Scientific Computing"
+        quote: "People say nothing is impossible, but I do nothing every day.",
+        name: "A.A. Milne"
     },{
-        quote: "When I first started using the phrase software engineering, it was considered to be quite amusing. They used to kid me about my radical ideas. Software eventually and necessarily gained the same respect as any other discipline.",
-        name: "Margaret Hamilton, Mathematician and Pioneering Computer Scientist"
+        quote: "Better to remain silent and be thought a fool than to speak out and remove all doubt.",
+        name: "Abraham Lincoln"
     },{
-        quote: "We will always have STEM with us. Some things will drop out of the public eye and will go away, but there will always be science, engineering, and technology.And there will always, always be mathematics.",
-        name: "Katherine Johnson, NASA Mathematician"
+        quote: "Light travels faster than sound. This is why some people appear bright until you hear them speak",
+        name: "Alan Dundes"
     },{
-        quote: "Innovation comes out of great human ingenuity and very personal passions.",
-        name: "Megan Smith, 3rd Chief Technology Officer of the United States"
+        quote: "The difference between stupidity and genius is that genius has its limits",
+        name: "Albert Einstein"
     },{
-        quote: "Sciences provide an understanding of a universal experience, Arts are a universal understanding of a personal experience...they are both a part of us and a manifestation of the same thing...the arts and sciences are avatars of human creativity",
-        name: "Mae Jemison"
+        quote: "But the fact that some geniuses were laughed at does not imply that all who are laughed at are geniuses. They laughed at Columbus, they laughed at Fulton, they laughed at the Wright Brothers. But they also laughed at Bozo the Clown.",
+        name: "Carl Sagan"
     },{
-        quote: "To me programming is more than an important practical art. It is also a gigantic undertaking in the foundations of knowledge.",
-        name: "Grace Hopper"
+        quote: "He who laughs last didn’t get the joke",
+        name: "Charles de Gaulle"
     },{
-        quote: "I was taught that the way of progress was neither swift nor easy.",
-        name: "Marie Curie"
+        quote: "Do not take life too seriously. You will never get out of it alive.",
+        name: "Emo Philips"
     },{
-        quote: "Machines take me by surprise with great frequency.",
+        quote: "A computer once beat me at chess, but it was no match for me at kick boxing..",
         name: "Alan Turing, Mathematician and Computer Scientist"
     },{
-        quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-        name: "Benjamin Franklin, Founding Father and Inventor"
+        quote: "You tried your best and you failed miserably. The lesson is ‘never try'.",
+        name: "Homer Simpson"
     },{
-        quote: "The computer was born to solve problems that did not exist before.",
-        name: "Bill Gates, Microsoft Co-Founder"
-    }
+        quote: "We are all here on earth to help others. What on earth the others are here for I don’t know.",
+        name: "W. H. Auden"
 ]
 
-// This creates a function that generates a random number so that we can display a random quote from our quotes variable. We are using 10 in our return because we have 10 quotes so we want a number from 0 to 9. The quotes array starts counting at 0.
 
 function randomNumber(){
     return Math.floor(Math.random() * 10)
 }
 
-// This creates a function that will take the random number we generate above, find a quote from our quotes array and then build the HTML for the quote to display on the page.
 
-function generateRandomQuote(){
-    // This will return an object of a randomly picked quote.
+
+function generateRandomQuote(){    
     let randomQuote = quotes[randomNumber()];
     return '<p class="quote">' + randomQuote.quote + '</p><p class="name">' + randomQuote.name + '</p>';
 }
 
-// This is jQuery (as indicated by the use of the $ sign). This code is watching for an event. the .on method allows you specify various events. We are using 'click', so the code inside of this function will run when a user clicks. After they click, we are targeting the HTML element were we want to place the quote and then we are updating the HTML to include the HTML we a returning from the generateRandomQuote function.
 
 $(".generate-quote").on("click", function () {
     $('#quote').html(generateRandomQuote())
